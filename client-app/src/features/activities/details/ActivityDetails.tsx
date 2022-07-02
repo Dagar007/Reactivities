ba9d37aca12f4067e315ed76ca077fededc0e034
Deepak Kumar <dagardeepak88@gmail.com>
2022-07-02 15:53:40 +0530

    End of Section 7

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import { observer } from 'mobx-react-lite';
 import { Button, ButtonGroup, Card, Image } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { useStore } from '../../../app/stores/store'
 
-interface Props {
-    activity: Activity;
-    cancelSelectActivity: ()=> void;
-    openForm: (id: string) => void;
-}
 
-export default function ActivityDetails({activity, cancelSelectActivity, openForm }: Props) {
+export default observer(function ActivityDetails() {
+
+  const {activityStore} = useStore();
+  const {selectedActivity: activity, cancelSelectedActivity, openForm} = activityStore
+
+  if(!activity) return <LoadingComponent content='Loading...'/>;
   return (
     <Card fluid>
-    <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+    <Image src={`/assets/categoryImages/${activity?.category}.jpg`} />
     <Card.Content>
-      <Card.Header>{activity.title}</Card.Header>
+      <Card.Header>{activity?.title}</Card.Header>
       <Card.Meta>
         <span>{activity.date}</span>
       </Card.Meta>
@@ -24,9 +25,10 @@ export default function ActivityDetails({activity, cancelSelectActivity, openFor
     <Card.Content extra>
       <ButtonGroup widths={2}>
         <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit'/>
-        <Button onClick={cancelSelectActivity} basic color='grey' content='Cancel'/>
+        <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel'/>
       </ButtonGroup>
     </Card.Content>
   </Card>
   )
-}
+})
+
